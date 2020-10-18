656b74021903e62e3525e5855102097d0f4c9a3d
Apoorv Dixit <apoorvdikshit10@gmail.com>
2020-10-18 14:36:26 +0530

    prgm1.c

---
+++ prgm2.c
@@ -0,0 +1,21 @@
+/***INFIX TO POSTFIX CONVERSION***/
+#include<stdio.h>
+#include<stdlib.h>
+#define STACKSIZE 20
+struct stack
+{
+	int top;
+	char item[STACKSIZE];
+};
+void initialize(struct stack *s)
+{
+s->top=-1;	
+};
+int main(){
+	struct stack s;
+	char infix[20];
+	printf("\nENTER YOUR INFIX EXPRESSION:=>> ");
+	scanf("%s",infix);
+	
+}
+printf("dev brach\n");
